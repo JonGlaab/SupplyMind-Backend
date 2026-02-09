3152ab53246b822bb04dd5fb9d9ab08da22fcc4c
fzmehrizi <65924618+fzmehrizi@users.noreply.github.com>
2026-02-09 13:32:20 -0500

    change default page size

--- src/main/java/com/supplymind/platform_core/common/util/PaginationDefaults.java
+++ src/main/java/com/supplymind/platform_core/common/util/PaginationDefaults.java
@@ -3,7 +3,7 @@ package com.supplymind.platform_core.common.util;
 public final class PaginationDefaults {
     private PaginationDefaults() {}
 
-    public static final int DEFAULT_PAGE_SIZE = 50;
+    public static final int DEFAULT_PAGE_SIZE = 10;
     public static final int MAX_PAGE_SIZE = 200;
     public static final String DEFAULT_SORT = "inventoryId";
 }
