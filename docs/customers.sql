043c16980130f1c63c26f15e05b5443abbd24248
Elizabeth Mokrusa <elizabethmokrusa@Lizs-MacBook-Pro.local>
2026-02-11 16:10:11 -0500

    Removed old .sql files as there have been a lot of changes to DB

--- docs/customers.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO defaultdb.customers (customer_id, name, email, address) VALUES (1, 'Tech Startups Inc', 'cust1@demo.com', '101 Innovation Dr');
-INSERT INTO defaultdb.customers (customer_id, name, email, address) VALUES (2, 'MegaCorp Logistics', 'cust2@demo.com', '500 Enterprise Way');
-INSERT INTO defaultdb.customers (customer_id, name, email, address) VALUES (3, 'Gadget World', 'corp@demo.com', '888 Retail Blvd');
--- docs/sales_orders.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO defaultdb.sales_orders (so_id, customer_id, warehouse_id, status) VALUES (1, 1, 1, 'DELIVERED');
-INSERT INTO defaultdb.sales_orders (so_id, customer_id, warehouse_id, status) VALUES (2, 1, 1, 'DELIVERED');
-INSERT INTO defaultdb.sales_orders (so_id, customer_id, warehouse_id, status) VALUES (3, 1, 1, 'DELIVERED');
-INSERT INTO defaultdb.sales_orders (so_id, customer_id, warehouse_id, status) VALUES (4, 2, 1, 'DELIVERED');
-INSERT INTO defaultdb.sales_orders (so_id, customer_id, warehouse_id, status) VALUES (5, 2, 1, 'DELIVERED');
-INSERT INTO defaultdb.sales_orders (so_id, customer_id, warehouse_id, status) VALUES (6, 3, 1, 'DELIVERED');
