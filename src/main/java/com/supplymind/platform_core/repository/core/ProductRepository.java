fae9356140528a1c75cc0a3d0c9423d3d36ba964
GlaabJ <jonglaab@gmail.com>
2026-02-13 18:42:54 -0500

    changed product service to not ddos ourelves

--- src/main/java/com/supplymind/platform_core/repository/core/ProductRepository.java
+++ src/main/java/com/supplymind/platform_core/repository/core/ProductRepository.java
@@ -1,11 +1,20 @@
 package com.supplymind.platform_core.repository.core;
 
 import com.supplymind.platform_core.model.core.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     boolean existsBySku(String sku);
     Optional<Product> findBySku(String sku);
+
+    @Query("SELECT p, SUM(COALESCE(i.qtyOnHand, 0)) " +
+            "FROM Product p " +
+            "LEFT JOIN Inventory i ON p.productId = i.product.productId " +
+            "GROUP BY p")
+    Page<Object[]> findAllWithTotalStock(Pageable pageable);
 }
